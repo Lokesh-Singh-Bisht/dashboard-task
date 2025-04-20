9ec41cc42d1e8e6aaeb742e3a87bba39179db206
Lokesh <lokesh@kuvaka.io>
2025-04-20 22:09:17 +0530

    deployment changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "96248104"
+    serviceWorkerVersion: "688526442"
   }
 });
